fbb54095968c994fe52ce98891eeffe8a1be1e49
sil <gabriel.silvas@intre.it>
2025-04-12 18:26:47 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { RouterView } from 'vue-router'
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import Menu from '@/components/Menu.vue'
+import Footer from '@/components/Footer.vue'
 
 // Import Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -25,40 +26,7 @@ const currentLocale = computed(() => locale.value)
       <RouterView />
     </div>
 
-    <footer class="bg-dark text-white py-4 mt-auto">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4 mb-3 mb-md-0">
-            <h5>SteelNovo Blacksmith</h5>
-            <p class="mb-0">{{ $t('footer.tagline') }}</p>
-            <p class="motto mt-2 font-italic">
-              <em>{{ $t('footer.motto') }}</em>
-            </p>
-            <p class="mb-0">{{ $t('footer.additionalInfo') }}</p>
-          </div>
-          <div class="col-md-4 mb-3 mb-md-0">
-            <h5>{{ $t('footer.contact') }}</h5>
-            <address class="mb-0">
-              <p>{{ $t('contact.info.address') }}</p>
-              <p>{{ $t('contact.info.phone') }}</p>
-              <p>{{ $t('contact.info.email') }}</p>
-            </address>
-          </div>
-          <div class="col-md-4">
-            <h5>{{ $t('footer.quickLinks') }}</h5>
-            <ul class="list-unstyled">
-              <li><router-link to="/gallery" class="text-white">{{ $t('nav.gallery') }}</router-link></li>
-              <li><router-link to="/about" class="text-white">{{ $t('nav.about') }}</router-link></li>
-              <li><router-link to="/contact" class="text-white">{{ $t('nav.contact') }}</router-link></li>
-            </ul>
-          </div>
-        </div>
-        <hr class="my-3">
-        <div class="text-center">
-          <p class="mb-0">{{ $t('footer.copyright') }}</p>
-        </div>
-      </div>
-    </footer>
+    <Footer />
   </div>
 </template>
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <footer class="mt-auto slim-footer">
+    <div class="bg-dark text-white py-3">
+      <div class="container text-center">
+        <p class="mb-0 p-3">{{ t('footer.companyName') }}</p>
+      </div>
+    </div>
+    <div class="bg-white text-dark p-5">
+      <div class="container text-center">
+        <div class="footer-links">
+          <router-link to="/gallery" class="footer-link">{{ t('nav.gallery') }}</router-link>
+          <router-link to="/about" class="footer-link">{{ t('nav.about') }}</router-link>
+          <router-link to="/contact" class="footer-link">{{ t('nav.contact') }}</router-link>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.slim-footer {
+  font-size: 12px;
+  width: 100%;
+}
+
+.footer-links {
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+  font-size: 12px;
+}
+
+.footer-link {
+  color: #333;
+  text-decoration: none;
+  transition: color 0.2s;
+  text-transform: uppercase;
+}
+
+.footer-link:hover {
+  color: #666;
+  text-decoration: underline;
+}
+</style>
